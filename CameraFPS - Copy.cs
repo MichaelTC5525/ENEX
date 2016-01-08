5dedc3a4de268c8dbb8213d092ac0e422be39a74
Michael Cheung <michael.cheung@live.ca>
2016-01-08 00:26:29 -0800

    Personal Scripting
    These are the self-made C# scripts in the original project.

---
+++ CameraFPS - Copy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFPS : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = player.transform.position + offset;
+		Perspective ();
+	}
+
+	void Perspective() {
+		float lookHorizontal = Input.GetAxis ("Mouse X");
+		float lookVertical = Input.GetAxis ("Mouse Y");
+		Vector3 sight = new Vector3 (lookVertical, lookHorizontal, 0f);
+		transform.Rotate (sight);
+	}
+}
